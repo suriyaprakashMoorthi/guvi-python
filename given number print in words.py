83bd2434905ba8b6376e787a0467d533d9617578
Prakash5598 <51951604+Prakash5598@users.noreply.github.com>
2019-08-03 12:07:57 +0530

    Update given number print in words.py

--- given number print in words.py
+++ given number print in words.py
@@ -1,3 +1,3 @@
-dic={1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine' ,10:'ten'}
+dic={1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine' ,10:'Ten'}
 n=int(input())
 print(dic[n])
